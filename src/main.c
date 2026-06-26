f3871245149b8e23d1a416996143a518f9bbce35
Nick <nik_usov.1998@mail.ru>
2026-06-27 01:34:26 +0500

    Add libjpeg in Makefile Add write_JPEG function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,53 @@
 #include <stdio.h>
+#include <jpeglib.h>
+#include <setjmp.h>
+#include <stdlib.h>
+
+extern JSAMPLE* image_buffer;
+extern int image_height;
+extern int image_width;
+
+GLOBAL(void)
+write_JPEG_file (char* filename, int quality)
+{
+  struct jpeg_compress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+
+  FILE* outfile;
+  JSAMPROW row_pointer[1];
+  int row_stride;
+
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_compress(&cinfo);
+
+  if ((outfile = fopen(filename, "wb")) == NULL) {
+    fprintf(stderr, "Can't open %s\n", filename);
+    exit(1);
+  }
+  jpeg_stdio_dest(&cinfo, outfile);
+  
+  cinfo.image_width = image_width;
+  cinfo.image_height = image_height;
+  cinfo.input_components = 3;
+  cinfo.in_color_space = JCS_EXT_RGB;
+
+  jpeg_set_defaults(&cinfo);
+  jpeg_set_quality(&cinfo, quality, TRUE);
+  jpeg_start_compress(&cinfo, TRUE);
+  row_stride = image_width * 3;
+  while (cinfo.next_scanline < cinfo.image_height) {
+    row_pointer[0] = &image_buffer[cinfo.next_scanline*row_stride];
+    (void)jpeg_write_scanlines(&cinfo, row_pointer, 1);
+  }
+  jpeg_finish_compress(&cinfo);
+  fclose(outfile);
+  jpeg_destroy_compress(&cinfo);
+
+}
 
 int main(int argc, char* argv[])
 {
+  write_JPEG_file("Test", 10);
   return 0;
 }
 
